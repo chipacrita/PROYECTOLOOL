cbfa35ed4ea74e0ef74d7401317406f94374c5a7
jonathan obando <alquingaandy2rod@gmail.com>
2026-04-09 09:04:08 -0500

    comido

---
+++ transporte.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+//1.Mediante la edad calcular el precio del boleto de avion
+//Precio del boleto edad (<18 50%, >=65 75%)
+//2.Calcular el valor del transporte terreste por km
+//(<=100 $5, <=200 $7.5, >=200 $10)
+
+public class transporte {
+    public static void main(String[] args) {
+    int option;
+    Scanner sc = new Scanner(System.in);
+    do{
+        System.out.println("MENU PRINCIPAL");
+        System.out.println("OPCION 1 - CALCULAR EL PRECIO DEL BOLETO DE AVION");
+        System.out.println("OPCION 2 - CALCULAR EL VALOR DEL TRANSPORTE TERRESTRE POR KM");
+        System.out.println("OPCION 3 - SALIR");
+        System.out.println("INGRESE UNA OPCION");
+        option = sc.nextInt();
+        switch (option) {
+
+            }
+        } while (option != 3);
+    }
+}
